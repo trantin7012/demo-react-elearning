b9a81eb6d0d64e19b035f3d99cbc52832946140f
Tin Tin <trantin7012@gmail.com>
2020-11-24 12:48:37 +0700

    Updates

---
+++ precache-manifest.99a5708a303c9f4bcaaa445f5cd75bb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eade75e20deead8aadd157c549452448",
+    "url": "/demo-react-elearning/index.html"
+  },
+  {
+    "revision": "bcc26f7e971cd02dd0d0",
+    "url": "/demo-react-elearning/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c4ccb7942ef6995b10a9",
+    "url": "/demo-react-elearning/static/js/2.3a36b587.chunk.js"
+  },
+  {
+    "revision": "1ab7518176c3809a2daa7d118a6f72b9",
+    "url": "/demo-react-elearning/static/js/2.3a36b587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc26f7e971cd02dd0d0",
+    "url": "/demo-react-elearning/static/js/main.8c977a0f.chunk.js"
+  },
+  {
+    "revision": "cfb00218a73b18d72ab6",
+    "url": "/demo-react-elearning/static/js/runtime-main.5aa4f19c.js"
+  },
+  {
+    "revision": "014b17db4a379b5f4ea7487142a74c8f",
+    "url": "/demo-react-elearning/static/media/khoahoc1.014b17db.jpg"
+  },
+  {
+    "revision": "86a8ea5efa0bde4a5f64939137c559f3",
+    "url": "/demo-react-elearning/static/media/khoahoc2.86a8ea5e.jpg"
+  },
+  {
+    "revision": "ba62d65915b039e49f702bc93bc7ed91",
+    "url": "/demo-react-elearning/static/media/khoahoc3.ba62d659.jpg"
+  }
+]);
